5622ce993f3f7a8c17acb89696336215c74ded4a
Shaojung <shaojung@gmail.com>
2016-10-27 14:32:22 +0800

    Dialog use for input

--- app/src/main/java/com/example/user/yvd102704/MainActivity.java
+++ app/src/main/java/com/example/user/yvd102704/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.yvd102704;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "按取消", Toast.LENGTH_SHORT).show();
             }
         });
+        builer.setNeutralButton("略過", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "按略過", Toast.LENGTH_SHORT).show();
+            }
+        });
         builer.show();
+    }
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("輸入");
+        builder.setMessage("請輸入資料:");
+        final EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "輸入為:" + ed.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
     }
 }
